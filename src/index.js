d28e34bd4fe38cc836aac3eaf69079be0f2e3696
Vigen <vigenh199@gmail.com>
2021-01-30 23:51:09 +0400

    Init commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('./db/mongoose');
+const userRoutes = require('./routes/user');
+const taskRoutes = require('./routes/task');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(userRoutes);
+app.use(taskRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+})
